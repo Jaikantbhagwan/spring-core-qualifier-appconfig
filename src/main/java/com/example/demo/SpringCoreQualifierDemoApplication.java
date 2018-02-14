ebf6844ca003d3a444c37d805411ee35f1ede7cb
Jaikant S <jaikant_singh@apple.com>
2018-02-14 12:46:06 +0530

    fresh code commit

---
+++ src/main/java/com/example/demo/SpringCoreQualifierDemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+//@SpringBootApplication
+public class SpringCoreQualifierDemoApplication {
+
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		//context.scan("com.example.demo");
+		
+		//context.refresh();
+		
+		VehicleService vehicle = context.getBean(VehicleService.class);
+		vehicle.service();
+		
+		context.close();
+	}
+}
